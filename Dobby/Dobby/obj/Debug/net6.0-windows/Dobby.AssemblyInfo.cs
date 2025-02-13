cc515730e78bb3a070bf78b2754790247da6cff8
yagovillargithub <91180046+yagovillargithub@users.noreply.github.com>
2025-02-13 15:21:35 +0100

    Unkown

--- Dobby/Dobby/obj/Debug/net6.0-windows/Dobby.AssemblyInfo.cs
+++ Dobby/Dobby/obj/Debug/net6.0-windows/Dobby.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dobby")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19f3d4b3daaa4d20874442856d55d3789011c240")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dobby")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dobby")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
